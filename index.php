c42b5eb3288c2f852e6c11524445c6e233df62ef
Unknown <ronaldofigueiredojr@hotmail.com>
2018-02-10 15:04:37 -0200

    Configurações Finais

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  require_once 'vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Demo</title>
+    <link rel="stylesheet" href="public/css/app.css">
+  </head>
+
+  <body>
+  <p>Hello World!!</p>
+    <script src="public/js/app.js"></script>
+  </body>
+</html>
